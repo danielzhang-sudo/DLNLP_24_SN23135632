dae464da3452453de3d96371c1be9dd0368d6b2b
danielzhang-sudo <uceexdz@ucl.ac.uk>
2024-05-09 01:10:41 +0100

    Add utils.py file

---
+++ B/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def to_latex(report):
+    df = pd.DataFrame.fromm_dict(report)
+    with open('mytable.tex', 'w') as f:
+        f.write(df.to_latex())
+
+def plot_figures(x_data, filename):
+    fig = plt.figure(figsize=(10, 7))
+    plt.plot(x_data)
+    fig.savefig(filename, dpi=fig.dpi)
