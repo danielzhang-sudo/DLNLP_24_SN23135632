0172005d0263355554d403525ee53a8346f2178c
danielzhang-sudo <147735541+danielzhang-sudo@users.noreply.github.com>
2024-05-10 07:33:47 +0100

    Update utils.py

--- B/utils.py
+++ B/utils.py
@@ -6,8 +6,10 @@ def to_latex(report):
     with open('mytable.tex', 'w') as f:
         f.write(df.to_latex())
 
-def plot_figures(x_data, filename):
-
+def plot_figures(data1, data2, filename):
     fig = plt.figure(figsize=(10, 7))
-    plt.plot(x_data)
-    fig.savefig(filename, dpi=fig.dpi)
+    plt.plot(data1, label='train')
+    plt.plot(data2, label='validation')
+    plt.title(filename)
+    plt.legend()
+    fig.savefig(filename+'.png', dpi=fig.dpi)
